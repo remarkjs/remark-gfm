0846014c5a5af1739f5470537c1eacb94da2c91c
Titus Wormer <tituswormer@gmail.com>
2021-08-06 11:57:33 +0200

    Remove warning for remark 12

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 import {gfm} from 'micromark-extension-gfm'
 import {gfmFromMarkdown, gfmToMarkdown} from 'mdast-util-gfm'
 
-let warningIssued
-
 export default function remarkGfm(options) {
   const data = this.data()
-
-  // Old remark.
-  /* c8 ignore next 14 */
-  if (
-    !warningIssued &&
-    ((this.Parser &&
-      this.Parser.prototype &&
-      this.Parser.prototype.blockTokenizers) ||
-      (this.Compiler &&
-        this.Compiler.prototype &&
-        this.Compiler.prototype.visitors))
-  ) {
-    warningIssued = true
-    console.warn(
-      '[remark-gfm] Warning: please upgrade to remark 13 to use this plugin'
-    )
-  }
 
   add('micromarkExtensions', gfm(options))
   add('fromMarkdownExtensions', gfmFromMarkdown)
