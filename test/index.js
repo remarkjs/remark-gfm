54b2ada84fda5dbbae8280579ee38e89c91b1586
Titus Wormer <tituswormer@gmail.com>
2021-08-06 12:06:09 +0200

    Update `xo`

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@
  * @typedef {import('../index.js').Options} Options
  */
 
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import test from 'tape'
 import {readSync} from 'to-vfile'
 import {unified} from 'unified'
